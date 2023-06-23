f060b9f10a8ad0801fe30081f9d2481cdc24780b
Torvaag <tfoinont@gmail.com>
2023-06-23 16:35:44 +0200

    ok

--- src/app/profil-admin/profil-admin.component.ts
+++ src/app/profil-admin/profil-admin.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-profil-admin',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./profil-admin.component.css']
 })
 export class ProfilAdminComponent {
+
+  candidats: any[] = [];
+  photoUrls: string[] = [];
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.getCandidats();
+  }
+
+  getCandidats(): void {
+    this.http.get<any[]>('http://localhost:8080/admin/profil/candidats').subscribe(candidats => {
+      this.candidats = candidats;
+    });
+  }
+
 
 }
