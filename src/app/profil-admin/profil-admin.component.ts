061b7dd73f0e580d427ed0abc414e9b3c3a26ac2
Torvaag <tfoinont@gmail.com>
2023-06-26 15:01:48 +0200

    ok

--- src/app/profil-admin/profil-admin.component.ts
+++ src/app/profil-admin/profil-admin.component.ts
@@ -9,6 +9,7 @@ import {HttpClient} from "@angular/common/http";
 export class ProfilAdminComponent {
 
   candidats: any[] = [];
+  selectedCandidatIndex: number | null = null; // Ajout de cette ligne
 
   constructor(private http: HttpClient) { }
 
@@ -32,6 +33,34 @@ export class ProfilAdminComponent {
   getFilename(path: string): string {
     return <string>path.split('/').pop();
   }
+  selectCandidat(index: number): void {
+    if (this.selectedCandidatIndex === index) {
+      this.selectedCandidatIndex = null;
+    } else {
+      this.selectedCandidatIndex = index;
+    }
+  }
+
+  accepterCandidature(candidat: any): void {
+    const idPersonne = candidat.personne.id;
+    this.http.put('http://localhost:8080/admin/accepter-candidature/' + idPersonne, {}).subscribe(() => {
+      candidat.accepte = true;
+      this.candidats.splice(this.candidats.indexOf(candidat), 1); // Supprimer le candidat de la liste
+    }, (error) => {
+      // Gérer les erreurs de la requête
+    });
+  }
+
+  refuserCandidature(candidat: any): void {
+    const idPersonne = candidat.personne.id;
+    this.http.put('http://localhost:8080/admin/refuser-candidature/' + idPersonne, {}).subscribe(() => {
+      candidat.accepte = true;
+      this.candidats.splice(this.candidats.indexOf(candidat), 1); // Supprimer le candidat de la liste
+    }, (error) => {
+      // Gérer les erreurs de la requête
+    });
+  }
+
 
 
 }
